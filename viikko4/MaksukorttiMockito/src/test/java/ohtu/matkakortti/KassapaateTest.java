24ddfb3feafa92fa11c5b4f0f684937f98f6780d
Janne <janne.penttala@helsinki.fi>
2022-03-29 06:17:46 +0300

    MaksukorttiMockito tehty

--- viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -40,5 +40,22 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void kortilleLadataanJosPositiivinenSumma() {
+        when(kortti.getSaldo()).thenReturn(0);
+        kassa.lataa(kortti, 5);
+        
+        verify(kortti, times(1)).lataa(5);
+        verify(kortti).lataa(eq(5));
+    }
+    
+    @Test
+    public void kortilleEiLadataJosNegatiivinenSumma() {
+        when(kortti.getSaldo()).thenReturn(0);
+        kassa.lataa(kortti, -5);
+        
+        verify(kortti, times(0)).lataa(-5);
+    }
       
 }
