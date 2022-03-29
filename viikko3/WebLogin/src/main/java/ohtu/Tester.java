b3476cee56005d554d921abec3c010bd1a5e6bf6
Janne <janne.penttala@helsinki.fi>
2022-03-29 15:43:09 +0300

    chromedriver käyttöön

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,15 +1,14 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Tester {
 
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
@@ -18,19 +17,48 @@ public class Tester {
         
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
-
+        
         sleep(2);
-
+                
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        element.sendKeys("pekka");
         element = driver.findElement(By.name("login"));
+        element.click();
         
         sleep(2);
-        element.submit();
+        
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+        
+        sleep(2);
+        
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
 
-        sleep(3);
+        sleep(2);
+        
+        Random r = new Random();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka"+r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("signup"));
+        
+        sleep(2);
+        
+        element.click();
+        
+//        sleep(2);
+//        
+//        element = driver.findElement(By.name("course page"));
+//        element.click();
+
+        sleep(2);
         
         driver.quit();
     }
